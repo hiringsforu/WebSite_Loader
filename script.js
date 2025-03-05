b3720ae54d9161c5bd8dfe392f79e8947d6c0954
hiringsforu <hiringsforu24@gmail.com>
2025-03-05 15:15:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    // Simulate a delay for demonstration purposes
+    setTimeout(function () {
+        document.body.classList.add('loaded');
+    }, 5000); // Adjust the delay as needed
+});
